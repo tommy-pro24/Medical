bc418587d6cbdb7c568a9f6b42b56673a9c57864
tommy-pro24 <devtommy1031@gmail.com>
2025-05-21 23:14:01 +0800

    complete the dashboard page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,24 +3,99 @@ import "react-date-range/dist/theme/default.css";
 import { DateRangePicker } from "@/components/ui/date-range-picker"
 import TotalBox from "@/components/dashboard/totalBox";
 import DashboardBarChart from "@/components/dashboard/DashboardBarChart";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useData } from "@/context/DataContext";
 import { useWebSocketContext } from '@/context/WebSocketContext';
 import { DateRange } from "react-day-picker";
 import React from "react";
 import { subDays } from "date-fns";
+import { request } from "@/lib/request";
+import TotalProduct from "@/components/ui/totalProduct";
+import LowProducts from "@/components/ui/lowProducts";
+import Orders from "@/components/ui/orders";
+import Transit from "@/components/ui/transit";
 
 export default function Home() {
   const { getCurrentUser } = useData();
   const { sendMessage } = useWebSocketContext();
+  const [cards, setCards] = useState([
+    {
+      title: "Total Products",
+      value: "0 types, 0 pieces",
+      desc: "Items in inventory",
+      Icon: TotalProduct,
+      style: 0
+    },
+    {
+      title: "Low Stock Alert",
+      value: "0 types",
+      desc: "Items need restock",
+      Icon: LowProducts,
+      style: 1
+    },
+    {
+      title: "Stock in",
+      value: "0 pieces",
+      desc: "Items in inventory",
+      Icon: TotalProduct,
+      style: 0
+    },
+    {
+      title: "Stock out",
+      value: "0 pieces",
+      desc: "Items in inventory",
+      Icon: TotalProduct,
+      style: 0
+    },
+  ]);
   const [date, setDate] = React.useState<DateRange | undefined>({
     from: subDays(new Date(), 7),
     to: new Date(),
   })
+
+  const [orderCards, setOrderCards] = useState([
+    {
+      title: "Pending",
+      value: "5",
+      desc: "Awaiting processing",
+      Icon: Orders,
+      style: 2
+    },
+    {
+      title: "Dispatched",
+      value: "5",
+      desc: "Currently in delivery",
+      Icon: Transit,
+      style: 3
+    },
+    {
+      title: "in-transit",
+      value: "5",
+      desc: "Completed deliveries",
+      Icon: Orders,
+      style: 4
+    },
+    {
+      title: "confirmed",
+      value: "5",
+      desc: "Completed deliveries",
+      Icon: Orders,
+      style: 4
+    },
+    {
+      title: "cancelled",
+      value: "5",
+      desc: "Completed deliveries",
+      Icon: Orders,
+      style: 4
+    }
+  ])
+
+
   useEffect(() => {
-    onLoad();
+    if (getCurrentUser() && date) onLoad();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [getCurrentUser(), date]);
 
   const onLoad = async () => {
     sendMessage({
@@ -30,6 +105,65 @@ export default function Home() {
       },
       timestamp: Date.now(),
     });
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const response: any = await request({
+      method: "POST",
+      url: '/dashboard/getData',
+      data: {
+        from: date?.from,
+        to: date?.to
+      },
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+
+    if (response) {
+
+      setCards(prevCards => [
+        {
+          ...prevCards[0],
+          value: `${response.totalProductTypes} types, ${response.totalProducts} pieces`
+        },
+        {
+          ...prevCards[1],
+          value: `${response.lowStockProductTypes} types`
+        },
+        {
+          ...prevCards[2],
+          value: `${response.stockCounts?.['stock-in']?.typeCount ?? 0} types, ${response.stockCounts?.['stock-in']?.quantity ?? 0} pieces`
+        },
+        {
+          ...prevCards[3],
+          value: `${response.stockCounts?.['stock-out']?.typeCount ?? 0} types, ${response.stockCounts?.['stock-out']?.quantity ?? 0} pieces`
+        }
+      ]);
+
+      setOrderCards(prevCards => [
+        {
+          ...prevCards[0],
+          value: `${response?.orders?.pending}`
+        },
+        {
+          ...prevCards[1],
+          value: `${response?.orders?.dispatched}`
+        },
+        {
+          ...prevCards[2],
+          value: `${response?.orders?.transit}`
+        },
+        {
+          ...prevCards[3],
+          value: `${response?.orders?.confirmed}`
+        },
+        {
+          ...prevCards[4],
+          value: `${response?.orders?.cancelled}`
+        }
+      ])
+
+    }
   };
 
   return (
@@ -41,11 +175,11 @@ export default function Home() {
       <div className="flex flex-col gap-10 sm:gap-16 md:gap-20">
         <div className="flex flex-col px-2 pb-3 border-b border-b-[#ffffff49]">
           <p className="text-gray-100 font-bold text-sm sm:text-[15px] ml-2 md:ml-6 c459m cbtcb">Inventory</p>
-          <TotalBox />
+          <TotalBox cards={cards} />
         </div>
         <div className="flex flex-col px-2 pb-3 border-b border-b-[#ffffff49]">
           <p className="text-gray-100 font-bold text-sm sm:text-[15px] ml-2 md:ml-6 c459m cbtcb">Orders</p>
-          <TotalBox />
+          <TotalBox cards={orderCards} />
         </div>
       </div>
       <div className="flex w-full overflow-x-auto">
